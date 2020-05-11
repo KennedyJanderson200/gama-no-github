e7f8ee56dcb2a48861ea7a794df7dcffb78a4103
KennedyJanderson200 <37151492+KennedyJanderson200@users.noreply.github.com>
2020-05-10 22:30:21 -0300

    Airbn
    Airbn

---
+++ javascript.js
@@ -0,0 +1,32 @@
+//funções javascript
+
+//function iniciar(){
+
+
+//document.querySelector("#paragrafo").onclick=alertar;
+
+
+
+
+
+
+//}
+
+//function alertar(){
+
+  //alert("ola a todos.");
+
+
+
+//}
+
+
+
+
+
+
+
+
+
+
+
